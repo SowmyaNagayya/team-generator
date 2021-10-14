ec867517f16cebe6702e8aca6f8245ee5402cd9d
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-10-14 16:39:39 -0500

    lib folder updated

---
+++ index.js
@@ -0,0 +1,101 @@
+//Importing Inquirer for user input and fs to write the HTML file.
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+//Importing question arrays to use with inquirer
+const engineerquestions = require('./src/engineerquestions.js');
+const internquestions = require('./src/internquestions.js');
+const managerquestions = require('./src/managerquestions.js');
+
+//Importing classes of team members
+const Engineer = require('./lib/Employee.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
+
+//geretare team members in html for specific team member
+const generateTeamMemberHTML = ({Engineer,Intern,Manager}) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+
+    <!-- Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Materialize JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        
+    <!-- Material Icon Library -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./style.css">
+    <title>My Team</title>
+
+</head>
+<body>
+  <!-- Navigation bar for My Team -->
+  <nav>
+    <div class="nav-wrapper">
+      <a class="brand-logo center">My Team</a>
+    </div>
+  </nav>
+
+  <main class="container">
+  <div class="row">
+
+            
+
+  <div class="col s12 m6 l4">
+    <div class="card blue darken-1">
+        <div class="card-content white-text">
+          <h6>Manager</h6>
+            <ul class="collection black-text">
+                <li class="collection-item">Team ID: </li>
+                <li class="collection-item">Email: <a href="mailto: manager@gmail.com"></a></li>
+                <li class="collection-item">Office Number:</li>
+            </ul>
+        </div>
+    </div>
+  </div>
+
+  <div class="col s12 m6 l4">
+    <div class="card blue darken-1">
+        <div class="card-content white-text">
+          <h6>Engineer</h6>
+            <ul class="collection black-text">
+                <li class="collection-item">Team ID: </li>
+                <li class="collection-item">Email: <a href="mailto: sowmya@gmail.com"></a></li>
+                <li class="collection-item">GitHub: <a href="https://github.com/SowmyaNagayya  "></a></li>
+            </ul>
+        </div>
+    </div>
+  </div>
+
+  <div class="col s12 m6 l4">
+    <div class="card blue darken-1">
+        <div class="card-content white-text"> 
+          <h6>Intern</h6>
+            <ul class="collection black-text">
+                <li class="collection-item">Team ID: </li>
+                <li class="collection-item">Email: <a href="mailto: abc@email.com"></a></li>
+                <li class="collection-item">School: </li>
+            </ul>
+        </div>
+    </div>
+  </div>
+  </div>
+  </main>
+
+</body>
+</html>`
+
+.then((answers) => {
+    const htmlPageContent = generateTeamMemberHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
